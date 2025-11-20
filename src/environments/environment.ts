1a87269d46cd1f67ebe4b5ac5b13f3b7f80e3ee8
UtpAlfonso <U22224607@utp.edu.pe>
2025-11-20 03:03:06 -0500

    Cambio de link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://jugueteria-api.onrender.com//api/v1', // La URL de tu backend de Spring Boot local
+  apiUrl: 'https://jugueteria-api.onrender.com', // La URL de tu backend de Spring Boot local
   mercadopagoPublicKey: 'APP_USR-48a1adfd-8cec-4495-a67f-d5e418ab84d4' // La URL de tu backend de Spring Boot local
 };
