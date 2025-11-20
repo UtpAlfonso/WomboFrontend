777108044908a7071ddc538226f06e143aa78dd8
UtpAlfonso <U22224607@utp.edu.pe>
2025-11-19 22:42:31 -0500

    Recolocacion de enviroments

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   
   // La URL de tu backend desplegado en Render (¡sin /api/v1 al final!)
-  apiUrl: 'https://jugueteria-api.onrender.com', 
+  apiUrl: 'https://jugueteria-api.onrender.com//api/v1', 
   
   // La PUBLIC KEY de PRUEBA de tu cuenta de vendedor de Mercado Pago
   mercadopagoPublicKey: 'APP_USR-48a1adfd-8cec-4495-a67f-d5e418ab84d4'
